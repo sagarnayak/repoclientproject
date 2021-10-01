317b8b1f84f6741e5736f9fb94dbccc5b131afcc
Stark <sagar@agpaytech.co.uk>
2021-10-01 10:34:32 +0530

    update

---
+++ src/main/kotlin/com/sagar/clientproject/ClientprojectApplication.kt
@@ -0,0 +1,11 @@
+package com.sagar.clientproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClientprojectApplication
+
+fun main(args: Array<String>) {
+    runApplication<ClientprojectApplication>(*args)
+}
---
+++ src/test/kotlin/com/sagar/clientproject/ClientprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sagar.clientproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClientprojectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
